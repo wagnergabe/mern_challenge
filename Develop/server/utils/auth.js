f0eb696f355490f55ce39c4415655cf1ebd93612
GSvenny <gabrielswagner7@gmail.com>
2022-09-23 20:22:20 -0500

    updated token auth middleware

--- Develop/server/utils/auth.js
+++ Develop/server/utils/auth.js
@@ -6,9 +6,9 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function ({req}) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.query.token || req.headers.authorization || req.body.token;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
