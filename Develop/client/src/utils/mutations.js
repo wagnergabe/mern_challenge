863ab42c54d3867b505f54174fc70ce29006f0c3
GSvenny <gabrielswagner7@gmail.com>
2022-09-24 21:30:30 -0500

    create mutation.js file, add plans

---
+++ Develop/client/src/utils/mutations.js
@@ -0,0 +1,6 @@
+import { gql } from '@apollo/client'
+
+//LOGIN_USER: will execute the loginUser mutation set up using Apollo Server
+//ADD_USER: will execute the addUser mutation
+//SAVE_BOOK will execute the saveBook mutation
+//REMOVE_BOOK will execute the removeBook mutation
